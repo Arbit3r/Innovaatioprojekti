ec3f39600aa1a671a031f2b12dd15c20dc955852
Arbit3r <rikukoski2@gmail.com>
2024-03-12 11:50:37 +0200

    Changed functions to follow connection.js structure

--- ReactNativeApp/components/signalingServer.js
+++ ReactNativeApp/components/signalingServer.js
@@ -1,8 +1,38 @@
-import { WebSocketServer } from 'ws';
+const { WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let candidates = [];
+let room = [];
+let nurse = [];
+
+function  handelConn(json) {
+
+    let response;
+    let isRoom = json.isRoom && JSON.parse(json.isRoom);
+
+    if(isRoom){
+        room.forEach((candidate) =>{
+            if (json.roomCode === candidate.roomCode){
+                console.log("found room " + candidate.webSocket);
+                response = candidate.webSocket;
+            }
+        });
+
+    }else if(!isRoom){
+        nurse.forEach((candidate) =>{
+            if (json.roomCode === candidate.roomCode){
+                console.log("found nurse " + candidate.webSocket);
+                response = candidate.webSocket;
+            }
+        });
+
+    } else {
+        console.log("Socket not found!");
+        response = null;
+    }
+
+    return response;
+}
 
 wss.on('connection', ws => {
 
@@ -14,56 +44,69 @@ wss.on('connection', ws => {
 
         switch (json.type){
 
-            //Make a call by searching roomCode and if found send offer to receiving end
-            case "call":
+            //Make a call by searching with ((roomCode)) and if found send offer to receiving end
+            case "offer":
 
-                console.log("Calling to " + json.search + "!");
-                candidates.forEach((candidate) =>{
-                    if (json.search === candidate.roomCode){
+                console.log("Calling to " + json.roomCode + "!");
 
-                        candidate.webSocket.send(JSON.stringify({type: "offer",id: json.id, offer: json.offer}));
+                console.log("Boolean: " + json.isRoom && JSON.parse(json.isRoom));
 
-                    }
-                });
+                try {
+                    handelConn(json).send(JSON.stringify(json));
+                }catch (e) {
+                    console.log(e);
+                }
+
                 break;
 
             //Answer by searching caller and sending answer data back to the caller
             case "answer":
 
-                console.log("Answering to " + json.search + " call!");
-                candidates.forEach((candidate) =>{
-                    if (json.search === candidate.roomCode){
-                        candidate.webSocket.send(JSON.stringify(json.answer));
-                    }
-                });
+                console.log("Answering to " + json.roomCode + " call!");
+                console.log("Boolean: " + json.isRoom && JSON.parse(json.isRoom));
 
+                try {
+                    handelConn(json).send(JSON.stringify(json));
+                }catch (e) {
+                    console.log(e);
+                }
                 break;
 
             //Register websocket with roomCode to the signaling server
-            case "regCandidate":
+            case "register":
 
-                let isIncluded = false;
+                if(handelConn(json) == null){
 
-                candidates.forEach((candidate) =>{
-                    if (json.roomCode === candidate.roomCode){
-                        isIncluded = true;
-                        console.log(json.roomCode +  " Is included!");
+                    console.log(json.roomCode + " Is not included!");
+
+                    if(json.isRoom === "true"){
+                        console.log("Pushing " + json.roomCode + " to room array!")
+                        room.push({
+                            roomCode: json.roomCode,
+                            webSocket: ws
+                        });
+                    } else if(json.isRoom === "false"){
+                        console.log("Pushing " + json.roomCode + " to nurse array!")
+                        nurse.push({
+                            roomCode: json.roomCode,
+                            webSocket: ws
+                        });
                     }
-                });
-
-                if(!isIncluded){
-                    console.log(json.roomCode + "Is not included!");
-                    candidates.push({
-                        roomCode: json.roomCode,
-                        webSocket: ws
-                    })
                 };
+                break;
+
+            case "candidate":
+
+                console.log("Sending candidate to " + json.roomCode);
+                try {
+                    handelConn(json).send(JSON.stringify(json));
+                }catch (e) {
+                    console.log(e);
+                }
 
                 break;
+
         };
-
-
-
 
         //wss.clients.forEach( function each(client){
         //client.send(data.toString());
@@ -74,7 +117,7 @@ wss.on('connection', ws => {
     ws.on('close', () => {
         console.log("disconnected");
     });
-
-
 });
+
+
 
